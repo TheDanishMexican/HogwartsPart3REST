bd00a83fbd19180fe34ee0f4c72dd8e3f5d8ec8b
TheDanishMexican <danieljchristjansen@gmail.com>
2024-02-13 17:58:00 +0100

    added error handling to get student

--- src/main/java/edu/hogwarts/studentadmin/controllers/StudentController.java
+++ src/main/java/edu/hogwarts/studentadmin/controllers/StudentController.java
@@ -3,12 +3,15 @@ package edu.hogwarts.studentadmin.controllers;
 import edu.hogwarts.studentadmin.models.Student;
 import edu.hogwarts.studentadmin.repositories.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class StudentController {
@@ -24,5 +27,12 @@ public class StudentController {
         List<Student> students = studentRepository.findAll();
 
         return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public ResponseEntity<Student> getStudent(@PathVariable int id) {
+        Optional<Student> student = studentRepository.findById(id);
+
+        return ResponseEntity.of(student);
     }
 }
