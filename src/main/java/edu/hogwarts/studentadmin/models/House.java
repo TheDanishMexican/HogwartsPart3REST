99d224641fae33ecb5790c582b2f03227ec16a5f
TheDanishMexican <danieljchristjansen@gmail.com>
2024-02-14 11:08:53 +0100

    started house entity

--- src/main/java/edu/hogwarts/studentadmin/models/House.java
+++ src/main/java/edu/hogwarts/studentadmin/models/House.java
@@ -1,4 +1,19 @@
 package edu.hogwarts.studentadmin.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.sql.Array;
+import java.util.List;
+
+@Entity
 public class House {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+//    private String[] colors = new String[2];
+
 }
